de02409132226346e395a3c76acb9c9956f58c2d
Nishtha Jain <nishthajain411@gmail.com>
2026-02-16 18:41:38 +0000

    Added new feature

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -3,3 +3,20 @@ const mongoose = require("mongoose");
 const Comment = mongoose.model("Comment");
 
 module.exports = router;
+ router.get("/", (req,res
+  ) =>{
+    Comment.find()
+    .then((comments)=>{
+        res.json(comments);
+    })
+    .catch((err)=> {
+        console.log(err);
+    });
+  }
+// add another endpoint for deleting a comment
+
+router.delete("/:commentId", (req,res,next)=> {
+    Comemnt.findByIdAndRemove(req.params.CommentId)
+    .then(()=>res.sendStatus(200))
+.catch(next);
+});
